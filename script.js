870209d0b1589dc395b3719f950d869723f83c19
sara.mcguinn <sara.mcguinn@gmail.com>
2020-07-18 19:59:07 -0500

    Add Clear button, Refine JS if/else logic & random syntax

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Assignment Code ------------------------------------------------------------------------
 var generateBtn = document.querySelector("#generate");
+var clearBtn = document.querySelector("#clear");
 var passwordLength
 var passwordNumbers
 var passwordCharacters
@@ -16,8 +17,10 @@ var passwordString = ""
 // Write password to the #password input --------------------------------------------------
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  if (password != undefined) {
+    passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
 }
 
 //Generate password that meets user's criteria --------------------------------------------
@@ -25,46 +28,47 @@ function generatePassword() {
 
   //Get input from user
   passwordLength = prompt("How many characters long should the password be? (min 8, max 128)");
-  passwordNumbers = confirm("Do you want to use numbers?");
-  passwordCharacters = confirm("Do you want to use special characters?");
-  passwordUppercase = confirm("Do you want to use uppercase letters?");
-  passwordLowercase = confirm("Do you want to use lowercase letters?");
-
   //Make sure password length fits parameters
   if(passwordLength >7 && passwordLength <129) {
+    passwordNumbers = confirm("Do you want to use numbers? (Click OK for Yes, Click Cancel for No)");
+    passwordCharacters = confirm("Do you want to use special characters? (Click OK for Yes, Click Cancel for No)");
+    passwordUppercase = confirm("Do you want to use uppercase letters? (Click OK for Yes, Click Cancel for No)");
+    passwordLowercase = confirm("Do you want to use lowercase letters? (Click OK for Yes, Click Cancel for No)");
+    //Make sure user chooses at least one set of characters
+    if(passwordNumbers||passwordCharacters||passwordUppercase||passwordLowercase) {
+      //Create array from which to generate password
+      if(passwordNumbers) {
+        fullArray = fullArray.concat(numbersArray);
+      }
+      if(passwordLowercase) {
+        fullArray = fullArray.concat(lowercaseArray);
+      }
+      if(passwordCharacters) {
+        fullArray = fullArray.concat(specialCharactersArray);
+      }
+      if(passwordUppercase) {
+        fullArray = fullArray.concat(uppercaseArray);
+      }
+      //Randomly choose an item from the full array, repeat 'passwordLength' number of times, each time add new item to the password array
+      for(i=0; i<passwordLength; i++) {
+        //choose random number between 0 and fullArray.length-1
+        random = Math.floor(Math.random() * fullArray.length);
+        //get that random number index from fullArray & add to passwordArray
+        passwordString = passwordString.concat(fullArray[random])
+      }
+      return passwordString;
+    } else {
+      alert("You need to choose at least one set: numbers, special characters, uppercase, or lowercase. Please try again.")
+    }
   } else {
-    alert("Please enter a password length between 8 and 128. Click Generate Password to try again.");
+    alert("You need to enter a password length between 8 and 128. Please try again.")
   }
+}
 
-  //Make sure user chooses at least one set of characters
-  if(passwordNumbers||passwordCharacters||passwordUppercase||passwordLowercase) {
-  } else {
-    alert("Please agree to use at least one set: numbers, special characters, uppercase, or lowercase. Click Generate Password to try again.")
-  }
-
-  //Create array from which to generate password
-  if(passwordNumbers) {
-    fullArray = fullArray.concat(numbersArray);
-  }
-  if(passwordLowercase) {
-    fullArray = fullArray.concat(lowercaseArray);
-  }
-  if(passwordCharacters) {
-    fullArray = fullArray.concat(specialCharactersArray);
-  }
-  if(passwordUppercase) {
-    fullArray = fullArray.concat(uppercaseArray);
-  }
-
-  //Randomly choose an item from the full array, repeat 'passwordLength' number of times, each time add new item to the password array
-  for(i=0; i<passwordLength; i++) {
-    //choose random number between 0 and fullArray.length-1
-    random = Math.floor((Math.random() * (fullArray.length - 1) ) + 1);
-    //get that random number index from fullArray & add to passwordArray
-    passwordString = passwordString.concat(fullArray[random])
-  }
-  return passwordString;
+function reloadPage() {
+  location.reload();
 }
 
 // Add event listener to generate button --------------------------------------------------
 generateBtn.addEventListener("click", writePassword);
+clearBtn.addEventListener("click", reloadPage);
